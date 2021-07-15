49e367e221cb45c1277812c35665af790c41624a
mayureshRewale <mayureshrewale@gmail.com>
2021-07-15 17:52:56 +0530

    Added diff.java

---
+++ diff.java
@@ -0,0 +1,7 @@
+public class sum{
+        public static void main(String){
+                int n1 = 10, n2 = 20, summ;
+                sum = n1-n2;
+                System.out.println(Diffis : " + summ);
+        }
+}
